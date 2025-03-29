a2d2151bbce9be2f8a6cbab4c39c87dd51c3742e
firefist420 <bearded_dad_69@outlook.com>
2025-03-29 18:13:33 -0400

    Deploy render

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,18 +1,14 @@
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-from app.database import Base
-from app.models import *
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app.models import Base
 
 config = context.config
 fileConfig(config.config_file_name)
-
 target_metadata = Base.metadata
 
 def run_migrations_offline():
@@ -27,14 +23,16 @@ def run_migrations_offline():
         context.run_migrations()
 
 def run_migrations_online():
+    configuration = config.get_section(config.config_ini_section)
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata
         )
         with context.begin_transaction():
             context.run_migrations()
