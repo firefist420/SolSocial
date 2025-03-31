6dd7e34079e3d0875d45a6179a14bf4df8ecf3d2
firefist420 <bearded_dad_69@outlook.com>
2025-03-31 08:30:11 -0400

    Cleaned

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\17327\Desktop\SolSocial-offical\venv
+set VIRTUAL_ENV=C:\Users\17327\Desktop\SolSocial-offical\SolSocial-clean\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
